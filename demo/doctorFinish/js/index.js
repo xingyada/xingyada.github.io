5a491b21dc94ca0ffe5dbb59825d0408886b0273
xingyada <xingyada@126.com>
2016-12-03 00:43:02 +0800

    20161203

---
+++ demo/doctorFinish/js/index.js
@@ -0,0 +1,30 @@
+$(function(){
+	$('.idxNavBtn').click(function(){
+		$('.mask').show();
+		
+	});
+	$('.mask li').click(function(){
+		$('.mask').hide();
+	});
+	
+	$('.maskVideoBg').click(function(){
+		$('.maskVideo').hide();
+		$('.MP4')[0].play();
+	});
+	$('.MP4').click(function(){
+		$('.MP4')[0].pause();
+		$('.maskVideo').show();
+	});	
+	//锚点
+
+	//产品轮播图
+    $('#nav .navList').click(function(){
+    	var iNow=$(this).index();
+    	$('#nav .navList').removeClass('cur');
+    	$('#nav .navList')[iNow].className='navList cur';
+    	swiper.slideTo(iNow, 1000, false);
+    });
+    
+   
+})
+// 
